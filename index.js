8649da76806cb56b0a201a194d1ca978e49c9981
abhinand <abhinandrkvatakara@gmail.com>
2019-07-05 10:24:46 +0530

    delete flow

--- index.js
+++ index.js
@@ -2,8 +2,9 @@ const express = require ('express');
 const exphbs = require ('express-handlebars');
 const bodyParser = require('body-parser');
 const mongoose = require ('mongoose');
-const methodOverride = require('method-override')
-
+const methodOverride = require('method-override');
+const session = require('express-session');
+const flash = require('connect-flash');
 const app = express();
 
 mongoose.connect('mongodb://localhost/vidjot-dev',{
@@ -21,6 +22,22 @@ app.engine('handlebars', exphbs({
 app.set('view engine', 'handlebars');
 app.use(methodOverride('_method'));
 
+app.use(session({
+    secret: 'secret',
+    resave: true,
+    saveUninitialized: true
+  }));
+
+app.use(flash());
+
+//Global variables
+app.use(function(req, res, next){
+  
+    res.locals.success_msg = req.flash('success_msg');;
+    res.locals.error_msg = req.flash('error_msg');
+    res.locals.error = req.flash('error');
+    next();
+}) ;
 
 //body parser middleware
 app.use(bodyParser.urlencoded({ extended: false }))
@@ -52,7 +69,7 @@ app.get('/ideas/edit/:id', (req, res) => {
 })
 
 app.get('/ideas', (req, res) => {
-    
+
     Idea.find({})
     .sort({date : 'desc'})
     .then(ideas => {
@@ -104,7 +121,17 @@ app.put('/ideas/:id', (req,res) => {
             res.redirect('/ideas');
         })
     })
-})
+});
+
+app.delete('/ideas/:id', (req,res) => {
+    Idea.deleteOne({
+        _id: req.params.id
+    })
+    .then(() => {
+        req.flash('success_msg','Video Idea has been removed');
+        res.redirect('/ideas');      
+    })
+});
 
 const port = 5000;
 app.listen(port,() => {
